05f82db5ebd61ce08cacb1c2845a722a2986eacb
Nick <nicksweetj@gmail.com>
2023-08-02 21:45:27 -0400

    removed items in index.js

--- models/index.js
+++ models/index.js
@@ -5,15 +5,16 @@ const User  = require('./user');
 const Posts = require('./post');
 const Comments = require('./comments');
 
-User.hasMany(Posts, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE',
-})
+// User.hasMany(Posts, {
+//     foreignKey: 'user_id',
+//     onDelete: 'CASCADE',
+// })
 
-User.hasMany(Comments, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE',
-})
+// User.hasMany(Comments, {
+//     foreignKey: 'user_id',
+//     onDelete: 'CASCADE',
+// })
+
 Posts.belongsTo(User, {
     foreignKey: 'user_id',
     onDelete: 'CASCADE'
@@ -23,14 +24,15 @@ Posts.hasMany(Comments, {
     foreignKey: 'post_id',
     onDelete: 'CASCADE'
 })
+
 Comments.belongsTo(User, {
     foreignKey: 'user_id',
     onDelete: 'CASCADE'
 })
 
-Comments.belongsTo(Posts, {
-    foreignKey: 'post_id',
-    onDelete: 'CASCADE',
-})
+// Comments.belongsTo(Posts, {
+//     foreignKey: 'post_id',
+//     onDelete: 'CASCADE',
+// })
 
 module.exports = { Comments, User, Posts }
