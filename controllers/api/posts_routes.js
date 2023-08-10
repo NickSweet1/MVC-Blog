9f5020f71cc6c3d5141460fdd44bde78778a4af3
Nick <nicksweetj@gmail.com>
2023-08-10 15:28:17 -0400

    added working get post and delete routes for posts, still need to work on login if in handlebars and logout functionality

--- controllers/api/posts_routes.js
+++ controllers/api/posts_routes.js
@@ -1,5 +1,39 @@
 const router = require('express').Router();
-const { Post } = require('../../models');
+const { Posts } = require('../../models');
 
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Posts.findAll();
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const postData = await Posts.create(req.body);
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const postData = await Posts.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
+        if (!postData) {
+            res.status(404).json({ message: 'No post found with this id!' });
+            return;
+        }
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
 
 module.exports = router;
